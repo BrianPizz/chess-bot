dbd5b61bb272bd64f3bafd510ee7876c63573d7c
Brian Pizzimenti <bpizzi2@wgu.edu>
2026-06-17 15:26:58 -0400

    add piece move function and legal verification

--- components/ChessBoard.tsx
+++ components/ChessBoard.tsx
@@ -1,11 +1,56 @@
 "use client";
 
 import { Chessboard } from "react-chessboard";
+import { Chess } from "chess.js";
+import { useState } from "react";
 
-function ChessBoardComponent() {
-    return(
-        <Chessboard />
-    )
+export default function ChessBoardComponent() {
+    // Store current game state
+  const [game, setGame] = useState(() => new Chess());
+
+  // when a piece is dropped, verifies move is legal
+  function onPieceDrop({
+    sourceSquare,
+    targetSquare,
+  }: {
+    sourceSquare: string;
+    targetSquare: string;
+  }) {
+    // Copy of current game
+    const gameCopy = new Chess(game.fen());
+
+    // Attempts to make move
+    const move = gameCopy.move({
+      from: sourceSquare,
+      to: targetSquare,
+      // Auto promote pawn to queen
+      promotion: "q",
+    });
+    // Null move is illegal
+    if (move === null) {
+      return false;
+    }
+    // Save updated game state and re-render the board
+    setGame(gameCopy);
+    // Return legal move
+    return true;
+  }
+
+  // Chessboard configurations 
+  const chessboardOptions = {
+    // Current board position as FEN string
+    position: game.fen(),
+    // Light square color
+    lightSquareStyle: {
+      backgroundColor: "#eeeed2",
+    },
+    // Dark square color
+    darkSquareStyle: {
+      backgroundColor: "#769656",
+    },
+    // pass move state/verification
+    onPieceDrop,
+  };
+
+  return <Chessboard options={chessboardOptions} />;
 }
-
-export default ChessBoardComponent;
